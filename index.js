ad5428e9d123d0834654e48f1b9ee5c5ae7173b3
reihan agam <reihanagam7@gmail.com>
2022-02-14 22:46:22 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const server = require('http').createServer()
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const multer = require('multer')
+const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, "./public/videos")
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname)
+  }
+})
+
+const upload = multer({
+  storage
+})
 
 io.on('connection', function (client) {
   console.log('Client Connect...', client.id);
 
   client.on('message', function name(data) {
-    io.emit('message', JSON.parse(data))
+    const result = JSON.parse(data)
+    io.emit('message', result)
   })
 
   client.on('disconnect', function () {
@@ -17,7 +35,28 @@ io.on('connection', function (client) {
   })
 })
 
-var port = 3000;
+app.use(cors())
+app.use(express.static("public"))
+
+app.post("/upload", upload.single("video"), (req, res) => {
+  let filename
+  if(req.file) {
+    filename = req.file.originalname
+    res.json({
+      "url": `http://192.168.113.73:3000/public/videos/${filename}`
+    })
+  } else {
+    res.json({
+      "url": ""
+    })
+  }
+})
+
+app.get("*", (req, res) => {
+  res.sendStatus(404)
+})
+
+const port = 3000;
 server.listen(port, function (e) {
   if (e) throw e
   console.log('Listening on port %d', port);
